722ca393ef32a1703c9a10b6f0d0660dfa8a5d44
rimobile1 <icaro.mo1234@gmail.com>
2019-08-07 22:01:33 -0300

    Exercicio

---
+++ dist/exercicio.js
@@ -0,0 +1,10 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var media = (nota1 + nota2 + nota3) / 3;
+if (media >= 7) {
+    console.log("Aprovado " + media);
+}
+else {
+    console.log("Reprovado" + media);
+}
---
+++ exercicio.ts
@@ -0,0 +1,12 @@
+let nota1: number = 10
+let nota2: number = 9 
+let nota3: number = 5
+let media= (nota1 + nota2 + nota3)/3
+
+if(media >=7){
+    console.log("Aprovado " + media)
+}
+else{
+    console.log("Reprovado" + media)
+
+}
